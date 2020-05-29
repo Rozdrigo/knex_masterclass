b724a7dd8baf9c140a898d75a703089faf59a680
Rozdrigo <rodrigocoelho236@gmail.com>
2020-05-29 15:14:40 -0300

    Finalizando CRUD de usuarios

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ const routes = require('./routes');
 
 const app = express();
 
-app.use(routes)
+app.use(express.json())
+app.use(routes);
+
+//catch all
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({ error: error.message});
+});
 
 app.listen(3333, () => {console.log('server is running')});
